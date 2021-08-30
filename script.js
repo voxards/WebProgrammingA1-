c3ff15a87f9b7dbfcf93c296c245ad9c3a6e6de0
voxards <80433673+voxards@users.noreply.github.com>
2021-08-30 14:04:11 +1000

    Update script.js

--- script.js
+++ script.js
@@ -10,10 +10,10 @@ const empstatus = document.getElementById("empstatus");
 form.addEventListener("submit", (e) => {
   e.preventDefault();
 
-  checkInputs();
+  validateForm();
 });
 
-function checkInputs() {
+function validateForm() {
   // get the values from the inputs
   const firstnameValue = firstname.value.trim();
   const lastnameValue = lastname.value.trim();
@@ -23,30 +23,48 @@ function checkInputs() {
   const studentstatusValue = studentstatus.value.trim();
   const empstatusValue = empstatus.value.trim();
 
+  // First name condition
   if (firstnameValue === "") {
     // show error
     // add error class (small)
-    setErrorFor(firstname, "*First name is required");
+    error(firstname, "*First name is required");
   } else {
     // add success class
-    setSuccessFor(firstname);
+    success(firstname);
   }
 
+  // Last name condtion
   if (lastnameValue === "") {
     // show error
     // add error class (small)
-    setErrorFor(lastname, "*Last name is required");
+    error(lastname, "*Last name is required");
   } else {
     // add success class
-    setSuccessFor(lastname);
+    success(lastname);
   }
 
+  // Email condition, including validation
+  if (emailValue === '') {
+    error(email, "*Email is required");
+  } else if (!validateEmail(emailValue)) {
+    error(email, "*Email is not valid");
+  } else {
+    success(email);
+  }
 
-
+  if (phoneValue === '') {
+    error(phone, "*Phone number is required");
+  } else if (!phoneValue.match(validatePhone)) {
+    error(phone, "*Phone number invalid, please use format 04xx xxx xxx")
+  } else {
+    success(phone);
+  }
 
 }
 
-function setErrorFor(input, message) {
+
+// Error function
+function error(input, message) {
   const formControl = input.parentElement; // form-control
   const small = formControl.querySelector("small");
 
@@ -57,7 +75,16 @@ function setErrorFor(input, message) {
   formControl.className = "form-control error";
 }
 
-function setSuccessFor(input) {
+// Success Function
+function success(input) {
   const formControl = input.parentElement;
   formControl.className = "form-control success";
 }
+
+// Validate email function
+function validateEmail(email) {
+  return /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/.test(email);
+}
+
+// Validate phone
+var validatePhone = /^\({0,1}((0|\+61)(2|4|3|7|8)){0,1}\){0,1}(\ |-){0,1}[0-9]{2}(\ |-){0,1}[0-9]{2}(\ |-){0,1}[0-9]{1}(\ |-){0,1}[0-9]{3}$/;
